bd70f0454bfffbe40566a70838fb3e9e6141d020
Paulo Alvarado <commonzenpython@gmail.com>
2012-05-28 23:41:39 -0400

    Added missing __init__.py

--- uploadit/views.py
+++ uploadit/views.py
@@ -4,8 +4,6 @@ from datetime import datetime
 
 
 from django.utils import simplejson
-from django.contrib import messages
-from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
