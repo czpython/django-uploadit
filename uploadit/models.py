be78c20d886016f1eef480f366d123bdcecab1a9
Paulo Alvarado <commonzenpython@gmail.com>
2012-05-28 23:43:54 -0400

    Converted indentation to spaces

--- uploadit/models.py
+++ uploadit/models.py
@@ -4,19 +4,19 @@ from django.contrib.contenttypes import generic
 
 
 def calc_img_path(instance, name):
-	slug = instance.parent.pk
-	return "%s/%s" % (slug, name)
+    slug = instance.parent.pk
+    return "%s/%s" % (slug, name)
 
 
 class UploadedFile(models.Model):
-	"""
-		Represents an uploaded file in the db.
-	"""
+    """
+        Represents an uploaded file in the db.
+    """
 
-	file = models.ImageField(upload_to=calc_img_path)
-	parent_type = models.ForeignKey(ContentType)
+    file = models.ImageField(upload_to=calc_img_path)
+    parent_type = models.ForeignKey(ContentType)
     parent_id = models.CharField(max_length=100)
-	parent = generic.GenericForeignKey('parent_type', 'parent_id')
+    parent = generic.GenericForeignKey('parent_type', 'parent_id')
 
-	def __unicode__(self):
-		return "%s - %s" %(self.file.name, str(self.parent))
+    def __unicode__(self):
+        return "%s - %s" %(self.file.name, str(self.parent))
