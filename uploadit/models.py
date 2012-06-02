04326ba8f96282c24920cc347384e120c74eaa84
Paulo Alvarado <commonzenpython@gmail.com>
2012-06-02 04:31:01 -0400

    Renamed function to be more general

--- uploadit/models.py
+++ uploadit/models.py
@@ -3,9 +3,9 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
 
-def calc_img_path(instance, name):
-    slug = instance.parent.pk
-    return "%s/%s" % (slug, name)
+def calc_file_path(instance, name):
+    folder = instance.parent.pk
+    return "%s/%s" % (folder, name)
 
 
 class UploadedFile(models.Model):
@@ -13,7 +13,7 @@ class UploadedFile(models.Model):
         Represents an uploaded file in the db.
     """
 
-    file = models.ImageField(upload_to=calc_img_path)
+    file = models.ImageField(upload_to=calc_file_path)
     parent_type = models.ForeignKey(ContentType)
     parent_id = models.CharField(max_length=100)
     parent = generic.GenericForeignKey('parent_type', 'parent_id')
